4c705be07df160408b85b21ef3faf254c9a35f0b
Nikita Shijan <nikusia0804@gmail.com>
2023-01-16 11:55:15 +1000

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import "normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCS_lv3_M7Kw8eOFA45HE3ciEnFGkDj0As",
+  authDomain: "subs-manager-9a705.firebaseapp.com",
+  projectId: "subs-manager-9a705",
+  storageBucket: "subs-manager-9a705.appspot.com",
+  messagingSenderId: "879889479417",
+  appId: "1:879889479417:web:1d1c7e098d21f17dbad428",
+  measurementId: "G-VLE1MYYBVF",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
